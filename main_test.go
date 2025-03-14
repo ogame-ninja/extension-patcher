822aa2ad26bceb5f3d8486536392131002ada7bc
notriv <notriv@ogame.ninja>
2025-03-14 00:12:43 -0700

    add test

--- main_test.go
+++ main_test.go
@@ -21,6 +21,7 @@ func Test_mustReplaceStr(t *testing.T) {
 	}{
 		{args: args{in: "ToReplace ToReplace ToReplace", old: "ToReplace", new: "ToReplaceNew", n: 3}, wantOut: "ToReplaceNew ToReplaceNew ToReplaceNew", name: "1"},
 		{args: args{in: "ToReplace ToReplace Some Other Text", old: "ToReplace", new: "ToReplaceNew", n: 2}, wantOut: "ToReplaceNew ToReplaceNew Some Other Text", name: "1"},
+		{args: args{in: "Some Other Text ToReplace ToReplace Some Other Text", old: "ToReplace", new: "ToReplaceNew", n: -1}, wantOut: "Some Other Text ToReplaceNew ToReplaceNew Some Other Text", name: "1"},
 		{args: args{in: "Some Other Text ToReplace ToReplace Some Other Text", old: "ToReplace", new: "ToReplaceNew", n: 2}, wantOut: "Some Other Text ToReplaceNew ToReplaceNew Some Other Text", name: "1"},
 		{args: args{in: "Some Other Text ToReplace ToReplace Some Other Text", old: "ToReplace", new: "ToReplaceNew", n: 1}, wantOut: "Some Other Text ToReplaceNew ToReplace Some Other Text", name: "1"},
 	}
